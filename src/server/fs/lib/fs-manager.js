b2c93c1d5d19329f283421b816cd8a9a4dd015f4
Kyungmi Koong <kyungmi.k@samsung.com>
2015-07-29 18:59:56 +0900

    [BUGFIX] always unlink temporary file created by writeFile(uploading) API

--- src/server/fs/lib/fs-manager.js
+++ src/server/fs/lib/fs-manager.js
@@ -1668,7 +1668,13 @@ router.post('/webida/api/fs/file/:fsid/*',
                 logger.info('file', name, file);
                 if (name !== 'file') {
                     logger.error('Bad upload request format: ', file.path);
-                    return res.sendfail(new ClientError(400, 'Bad upload request format'));
+                    Fs.unlink(file.path, function (cleanErr) {
+                        if (cleanErr) {
+                            logger.warn('Write File clean error: ', cleanErr);
+                        }
+                        res.sendfail(new ClientError(400, 'Bad upload request format'));
+                    });
+                    return;
                 }
                 if (file.size >= config.services.fs.uploadPolicy.maxUploadSize) {
                     return res.status(413).send('Uploading file is too large: ' + file.size + ' bytes');
@@ -1681,7 +1687,7 @@ router.post('/webida/api/fs/file/:fsid/*',
                         }
 
                         if (err) {
-                            logger.error('!!!!!!! form.parse->writeFile, error:', err);
+                            logger.error('write file error: ', err);
                             return res.sendfail(err);
                         }
                         logger.info('sessionID: ', fields.sessionID);
