b381b7bfc6b6f9b052d0e754ea265fea2dd5322a
Koong Kyungmi <kyungmi.k@samsung.com>
2015-09-11 12:47:49 +0900

    [IMPROVEMENT] change proxy implementation from `http-proxy` to `http-master`

--- src/server/proxy/proxy.js
+++ src/server/proxy/proxy.js
@@ -1,12 +1,12 @@
 /*
  * Copyright (c) 2012-2015 S-Core Co., Ltd.
- * 
+ *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
- * 
+ *
  *     http://www.apache.org/licenses/LICENSE-2.0
- * 
+ *
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
@@ -23,53 +23,109 @@ var baseSvc = require('../common/n-svc').Svc;
 
 var express = require('express');
 var fs = require('fs');
-var ejs = require('ejs');
 
 var ProxySvr = function (svc, svrName, conf) {
     baseSvr.call(this, svc, svrName, conf);
 
     this.httpServer = null;
     this.httpsServer = null;
-    this.httpProxy = require('http-proxy');
+    this.httpProxy = new (require('http-master'))();
+    this.httpProxy.on('logNotice', logger.info);
+    this.httpProxy.on('logError', logger.error);
 
     logger.info('ProxySvr constructor');
     extend(ProxySvr, baseSvr);
 };
 
-var errorTemplate;
+ProxySvr.prototype.loadConfig = function (cb) {
+    var self = this;
+    var config = global.app.config;
+    var jsfile = require('jsonfile');
+
+    jsfile.readFile(config.routingTablePath, function (err, table) {
+        if (err) {
+            logger.error('read routing table failed', err);
+            cb(err, null);
+        } else {
+            var conf = self.svc.config;
+            var forceHttps = conf.forceHttps;
+            var options = {
+                //workerCount: require('os').cpus().length,
+                ports: {},
+                errorHtmlFile: __dirname + '/views/error.ejs'
+            };
+
+            /* fill http options */
+            if (!forceHttps) {
+                if (conf.httpHost && conf.httpPort) {
+                    options.ports[conf.httpPort] = {
+                        router: table.router
+                    };
+                }
+            }
+
+            /* fill https options */
+            if (conf.httpsHost && conf.httpsPort) {
+                var caExist = fs.existsSync(config.sslCaPath);
+                var keyExist = fs.existsSync(config.sslKeyPath);
+                var certExist = fs.existsSync(config.sslCertPath);
+
+                if (caExist && keyExist && certExist) {
+                    options.ports[conf.httpsPort] = {
+                        router: table.router,
+                        ssl: {
+                            ca: config.sslCaPath,
+                            key: config.sslKeyPath,
+                            cert: config.sslCertPath
+                        }
+                    };
+                } else {
+                    logger.info('Can not find key or cert file. So does not listen https request');
+                }
+            }
+
+            logger.debug('options', JSON.stringify(options, null, 4));
+            cb(null, options);
+        }
+    });
+};
+
+ProxySvr.prototype.reload = function () {
+    var self = this;
+
+    logger.info('reload proxy due to config change');
+    self.loadConfig(function (err, options) {
+        if (err) {
+            logger.error('fail to load config. ignore config change...');
+        } else {
+            var startTime;
+            var proxy = self.httpProxy;
+            startTime = new Date().getTime();
+            /*
+             * http-master reload does not work properly.
+             * so close all servers before reload servers with new config.
+             */
+            proxy.reload({}, function() {
+                //if (e1) {
+                //    logger.error('failed to stop all servers', e1);
+                //}
+                proxy.reload(options, function (e) {
+                    if (e) {
+                        logger.error('failed to reload proxy. ignore error', e);
+                    }
+                    logger.info('proxy reloaded, downtime was ' +
+                            (new Date().getTime() - startTime) + 'ms');
+                });
+            });
+        }
+    });
+};
 
 ProxySvr.prototype.start = function () {
     var self = this;
     var conf = self.svc.config;
     var config = global.app.config;
-    var options = {
-        router: global.app.config.routingTablePath
-    };
     var forceHttps = conf.forceHttps;
-
-    function renderErrorPage(res, err){
-        res.writeHead(500, {'Content-Type': 'text/html'});
-        res.end(ejs.render(errorTemplate, {error: err}));
-    }
-
-    function createProxyServer(host, port, options) {
-        var server = self.httpProxy.createServer(host, options).listen(port);
-        server.proxy.on('proxyError', function (err, req, res) {
-            if(!errorTemplate) {
-                fs.readFile(__dirname + '/views/error.ejs', 'utf-8', function (fileError, template) {
-                    if (fileError) {
-                        res.writeHead(500, {'Content-Type': 'text/html'});
-                        res.end('Something went wrong. We will fix it as soon as possible.');
-                    } else {
-                        errorTemplate = template;
-                        renderErrorPage(res, err);
-                    }
-                });
-            } else {
-                renderErrorPage(res, err);
-            }
-        });
-    }
 
     if (forceHttps) {
         var http = express();
@@ -78,25 +134,25 @@ ProxySvr.prototype.start = function () {
             res.redirect('https://' + req.hostname + req.url);
         });
         http.listen(80);
-    } else {
-        createProxyServer(conf.httpHost,conf.httpPort, options);
     }
 
-    if (conf.httpsHost && conf.httpsPort) {
-        var keyExist = fs.existsSync(config.sslKeyPath);
-        var certExist = fs.existsSync(config.sslCertPath);
-
-        if (keyExist && certExist) {
-            options.https = {
-                ca: fs.readFileSync('/var/webida/keys/AlphaSSLroot.crt', 'utf8'),
-                key: fs.readFileSync(config.sslKeyPath, 'utf8'),
-                cert: fs.readFileSync(config.sslCertPath, 'utf8')
-            };
-            createProxyServer(conf.httpsHost,conf.httpsPort, options);
-        } else {
-            console.log('Can not find key or cert file. So does not listen https request');
+    self.loadConfig(function (err, options) {
+        if (err) {
+            logger.error('fail to load config');
+            throw err;
         }
-    }
+        self.httpProxy.init(options, function (err) {
+            if (err) {
+                logger.error('failed to start proxy');
+                throw err;
+            }
+
+            var watch = require('node-watch');
+            watch(config.routingTablePath, function () {
+                self.reload();
+            });
+        });
+    });
 };
 
 ProxySvr.prototype.stop = function () {
@@ -145,5 +201,3 @@ ProxySvc.prototype.stopped = function () {
 };
 
 exports.Svc = ProxySvc;
-
-
